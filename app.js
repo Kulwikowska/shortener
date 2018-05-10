8944992306ce7a32944f382dad6f14a907f1fb70
Agnieszka Kulwikowska <agnieszkakulwikowska@MacBook-Air-Agnieszka.local>
2018-05-10 12:09:49 +0200

    routes folder added

--- app.js
+++ app.js
@@ -4,75 +4,15 @@ var express = require("express"),
     mongoose = require("mongoose"),
     Link = require("./models/link")
 
+var linkRoutes = require("./routes/link")
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+app.use("/", linkRoutes);
 
 mongoose.connect("mongodb://localhost/myapp");
-
-
-
-// new route 
-app.get("/links/new", function(req, res) {
-    res.render("links/new");
-});
-
-
-
-
-app.post("/api/links", function(req, res) {
-    var url = req.body.link.url;
-    var newLink = { url: url }
-    Link.create(newLink, function(err, newlyCreated) {
-        if (err) {
-            console.log(err);
-        } else {
-            newlyCreated.shorten = newlyCreated._id.toString().substring(0, 7);
-            newlyCreated.save(function(err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(newlyCreated);
-                    res.render("index", { link: newlyCreated });
-                }
-            });
-        }
-    });
-});
-
-
-app.get("/:shorten", function(req, res) {
-    var shorten = req.params.shorten;
-    Link.findOne({ shorten: shorten }, function(err, link) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(link);
-            res.redirect(link.url);
-        }
-    });
-});
-
-//index route
-app.get("/", function(req, res) {
-    Link.find({}, function(err, allLinks) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("index", { allLinks: allLinks });
-        }
-    })
-});
-
-
-
-
-
-
-
-
-
 
 
 app.listen(8080, "127.0.0.1", function() {
---
+++ routes/link.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var router = express.Router();
+var Link = require("../models/link");
+
+//INDEX - show all links
+router.get("/", function(req, res) {
+    Link.find({}, function(err, allLinks) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { allLinks: allLinks });
+        }
+    })
+});
+
+// NEW - show form to create new shorted link
+router.get("/links/new", function(req, res) {
+    res.render("links/new");
+});
+
+
+// CREATE - show new link and short it
+router.post("/api/links", function(req, res) {
+    var url = req.body.link.url;
+    var newLink = { url: url }
+    Link.create(newLink, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            newlyCreated.shorten = newlyCreated._id.toString().substring(0, 7);
+            newlyCreated.save(function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(newlyCreated);
+                    res.render("index", { link: newlyCreated });
+                }
+            });
+        }
+    });
+});
+
+// shorted link redirect
+router.get("/:shorten", function(req, res) {
+    var shorten = req.params.shorten;
+    Link.findOne({ shorten: shorten }, function(err, link) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(link);
+            res.redirect(link.url);
+        }
+    });
+});
+
+module.exports = router;
